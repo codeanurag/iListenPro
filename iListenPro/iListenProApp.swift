ee9600e979b90ad4e2a8a8de1f94ecaf12243c80
codeanurag <codeanurag@users.noreply.github.com>
2025-07-03 15:21:23 +0530

    Initial commit: iOS project iListenPro

---
+++ iListenPro/iListenProApp.swift
@@ -0,0 +1,17 @@
+//
+//  iListenProApp.swift
+//  iListenPro
+//
+//  Created by Anurag Pandit on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iListenProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iListenProTests/iListenProTests.swift
@@ -0,0 +1,16 @@
+//
+//  iListenProTests.swift
+//  iListenProTests
+//
+//  Created by Anurag Pandit on 03/07/25.
+//
+
+import Testing
+
+struct iListenProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
